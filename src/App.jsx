234336cc4fcc91f3fe08a43ff6171eb5d38a5092
Vrishab N <vrishabnalothayyil@gmail.com>
2026-06-25 07:14:07 +0530

    Change 1 Cart gst values

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { router } from "./router";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default App;
